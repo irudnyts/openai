38afb7084686a2137f9b6c020c2bd8c07c16fb42
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-05-11 11:45:52 +0200

    Allow for organization id for engines functions

--- R/engines.R
+++ R/engines.R
@@ -1,7 +1,7 @@
 #' @export
 list_engines <- function(
-        openai_api_key = Sys.getenv("OPENAI_API_KEY")
-        # openai_organization = NULL
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
 ) {
 
     assertthat::assert_that(
@@ -9,15 +9,27 @@ list_engines <- function(
         assertthat::noNA(openai_api_key)
     )
 
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
     base_url <- "https://api.openai.com/v1/engines"
+
+    headers <- c(
+        Authorization = paste("Bearer", openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        headers[`OpenAI-Organization`] <- openai_organization
+    }
 
     result <- httr::GET(
         url = base_url,
         httr::content_type_json(),
-        httr::add_headers(
-            Authorization = paste("Bearer", openai_api_key, sep = " ")
-        ),
-        # httr::add_headers(`OpenAI-Organization` = openai_organization),
+        httr::add_headers(.headers = headers),
         encode = "json"
     )
 
@@ -40,24 +52,36 @@ retrieve_engine <- function(
 ) {
 
     assertthat::assert_that(
-        assertthat::is.string(openai_api_key),
-        assertthat::noNA(openai_api_key)
-    )
-
-    assertthat::assert_that(
         assertthat::is.string(engine),
         assertthat::noNA(engine)
     )
 
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
     base_url <- glue::glue("https://api.openai.com/v1/engines/{engine}")
+
+    headers <- c(
+        Authorization = paste("Bearer", openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        headers[`OpenAI-Organization`] <- openai_organization
+    }
 
     result <- httr::GET(
         url = base_url,
         httr::content_type_json(),
-        httr::add_headers(
-            Authorization = paste("Bearer", openai_api_key, sep = " ")
-        ),
-        # httr::add_headers(`OpenAI-Organization` = openai_organization),
+        httr::add_headers(.headers = headers),
         encode = "json"
     )
 
