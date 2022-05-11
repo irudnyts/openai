6e263c4c307f97b22ff87bc9a9e7e9106e51662c
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-05-11 10:59:19 +0200

    Add a function `retrieve_engine()` and rename the file to `engines.R`

---
+++ R/engines.R
@@ -0,0 +1,57 @@
+#' @export
+list_engines <- function(
+        openai_api_key = Sys.getenv("OPENAI_API_KEY")
+        # openai_organization = NULL
+) {
+
+    base_url <- "https://api.openai.com/v1/engines"
+
+    result <- httr::GET(
+        url = base_url,
+        httr::content_type_json(),
+        httr::add_headers(
+            Authorization = paste("Bearer", openai_api_key, sep = " ")
+        ),
+        # httr::add_headers(`OpenAI-Organization` = openai_organization),
+        encode = "json"
+    )
+
+    verify_mime_type(result)
+
+    httr::stop_for_status(result)
+
+    result %>%
+        httr::content(as = "text") %>%
+        jsonlite::fromJSON(flatten = TRUE) %>%
+        purrr::pluck("data")
+
+}
+
+#' @export
+retrieve_engine <- function(
+        engine,
+        openai_api_key = Sys.getenv("OPENAI_API_KEY")
+        # openai_organization = NULL
+) {
+
+    base_url <- glue::glue("https://api.openai.com/v1/engines/{engine}")
+
+    result <- httr::GET(
+        url = base_url,
+        httr::content_type_json(),
+        httr::add_headers(
+            Authorization = paste("Bearer", openai_api_key, sep = " ")
+        ),
+        # httr::add_headers(`OpenAI-Organization` = openai_organization),
+        encode = "json"
+    )
+
+    verify_mime_type(result)
+
+    httr::stop_for_status(result)
+
+    result %>%
+        httr::content(as = "text") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+}
