aabbfb0b4c948261ea6e5c827631e9a2a0c18b82
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-05-21 12:34:37 +0200

    Check the rest of arguments of `completeion()` and pass them to the body of request

--- R/completion.R
+++ R/completion.R
@@ -6,16 +6,16 @@ completion <- function(
         max_tokens = 16,
         temperature = 1,
         top_p = 1,
-        # n = 1,
-        # stream = FALSE,
-        # logprobs = NULL,
-        # echo = FALSE,
-        # stop = NULL,
-        # presence_penalty = 0,
-        # frequency_penalty = 0,
-        # best_of = 1,
-        # logit_bias = NULL,
-        # user = NULL,
+        n = 1,
+        stream = FALSE,
+        logprobs = NULL,
+        echo = FALSE,
+        stop = NULL,
+        presence_penalty = 0,
+        frequency_penalty = 0,
+        best_of = 1,
+        logit_bias = NULL,
+        user = NULL,
         openai_api_key = Sys.getenv("OPENAI_API_KEY"),
         openai_organization = NULL
         ) {
@@ -47,17 +47,78 @@ completion <- function(
     assertthat::assert_that(
         assertthat::is.number(temperature),
         assertthat::noNA(temperature),
-        between_zero_and_two(temperature)
+        value_between(temperature, 0, 2)
     )
 
     assertthat::assert_that(
         assertthat::is.number(top_p),
         assertthat::noNA(top_p),
-        between_zero_and_two(top_p)
+        value_between(top_p, 0, 2)
     )
 
+    if (both_specified(temperature, top_p)) {
+        warning(
+            "It is recommended NOT to specify temperature and top_p at a time."
+        )
+    }
 
-    ### XXX check if not specified
+    assertthat::assert_that(
+        assertthat::is.count(n)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.flag(stream),
+        assertthat::noNA(stream),
+        is_false(stream)
+    )
+
+    if (!is.null(suffix)) {
+        assertthat::assert_that(
+            assertthat::is.count(logprobs - 1),
+            value_between(logprobs, 0, 5)
+
+        )
+    }
+
+    assertthat::assert_that(
+        assertthat::is.flag(echo),
+        assertthat::noNA(echo)
+    )
+
+    if (!is.null(stop)) {
+        assertthat::assert_that(
+            is.character(stop),
+            assertthat::noNA(stop),
+            length_between(stop, 1, 4)
+        )
+    }
+
+    assertthat::assert_that(
+        assertthat::is.number(presence_penalty),
+        assertthat::noNA(presence_penalty),
+        value_between(presence_penalty, -2, 2)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.number(frequency_penalty),
+        assertthat::noNA(frequency_penalty),
+        value_between(frequency_penalty, -2, 2)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.count(best_of)
+    )
+
+    assertthat::assert_that(
+        best_of >= n
+    )
+
+    if (!is.null(user)) {
+        assertthat::assert_that(
+            assertthat::is.string(user),
+            assertthat::noNA(user)
+        )
+    }
 
     assertthat::assert_that(
         assertthat::is.string(openai_api_key),
@@ -95,6 +156,16 @@ completion <- function(
     body[["max_tokens"]] <- max_tokens
     body[["temperature"]] <- temperature
     body[["top_p"]] <- top_p
+    body[["n"]] <- n
+    body[["stream"]] <- stream
+    body[["logprobs"]] <- logprobs
+    body[["echo"]] <- echo
+    body[["stop"]] <- stop
+    body[["presence_penalty"]] <- presence_penalty
+    body[["frequency_penalty"]] <- frequency_penalty
+    body[["best_of"]] <- best_of
+    body[["logit_bias"]] <- logit_bias
+    body[["user"]] <- user
 
     #---------------------------------------------------------------------------
     # Make a request and verify its result
