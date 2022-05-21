0da4ada57bcebe1e4a39f146de8fdb4070b507fb
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-05-21 12:33:57 +0200

    Generalize assertions

--- R/utils-assertions.R
+++ R/utils-assertions.R
@@ -1,20 +1,58 @@
-between_zero_and_two <- function(x) {
-    x >= 0 && x <= 2
+#-------------------------------------------------------------------------------
+
+value_between <- function(x, lower, upper) {
+    x >= lower && x <= upper
 }
 
-assertthat::on_failure(between_zero_and_two) <- function(call, env) {
-    paste0(deparse(call$x), " is not between zero and two")
+assertthat::on_failure(value_between) <- function(call, env) {
+    paste0(
+        deparse(call$x),
+        " is not between ",
+        deparse(call$lower),
+        " and ",
+        deparse(call$upper)
+    )
 }
 
-between_zero_and_one <- function(x) {
-    x >= 0 && x <= 2
-}
+#-------------------------------------------------------------------------------
 
-assertthat::on_failure(between_zero_and_two) <- function(call, env) {
-    paste0(deparse(call$x), " is not between zero and one")
-}
-
-### XXX
-not_both_specified <- function(x, y) {
+both_specified <- function(x, y) {
     x != 1 && y != 1
+}
+
+#-------------------------------------------------------------------------------
+
+length_between <- function(x, lower, upper) {
+    length(x) >= lower && length(x) <= upper
+}
+
+assertthat::on_failure(length_between) <- function(call, env) {
+    paste0(
+        "Length of ",
+        deparse(call$x),
+        " is not between ",
+        deparse(call$lower),
+        " and ",
+        deparse(call$upper)
+    )
+}
+
+#-------------------------------------------------------------------------------
+
+is_false <- function(x) {
+    !x
+}
+
+assertthat::on_failure(is_false) <- function(call, env) {
+    paste0(deparse(call$x), " is not yet implemented.")
+}
+
+#-------------------------------------------------------------------------------
+
+is_null <- function(x) {
+    is.null(x)
+}
+
+assertthat::on_failure(is_null) <- function(call, env) {
+    paste0(deparse(call$x), " is not yet implemented.")
 }
