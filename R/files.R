18d31d317a8424be12345792307bf082c0a806bf
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-05-23 00:02:56 +0200

    Add function to upload and list files

---
+++ R/files.R
@@ -0,0 +1,144 @@
+#' @export
+list_files <- function(
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build parameters of the request
+
+    base_url <- "https://api.openai.com/v1/files"
+
+    headers <- c(
+        Authorization = paste("Bearer", openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        headers[`OpenAI-Organization`] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Make a request and verify its result
+
+    result <- httr::GET(
+        url = base_url,
+        httr::content_type_json(),
+        httr::add_headers(.headers = headers),
+        encode = "json"
+    )
+
+    verify_mime_type(result)
+
+    httr::stop_for_status(result)
+
+    #---------------------------------------------------------------------------
+    # Parse the result of the request
+
+    result %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE) %>%
+        purrr::pluck("data")
+
+}
+
+#' @export
+upload_file <- function(
+        file,
+        purpose = c("search", "answers", "classifications", "fine-tune"),
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+    purpose <- match.arg(purpose)
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        assertthat::is.string(file),
+        assertthat::noNA(file),
+        file.exists(file),
+        assertthat::is.readable(file)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(purpose),
+        assertthat::noNA(purpose)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build parameters of the request
+
+    base_url <- "https://api.openai.com/v1/files"
+
+    headers <- c(
+        Authorization = paste("Bearer", openai_api_key),
+        "Content-Type" = "multipart/form-data"
+    )
+
+    if (!is.null(openai_organization)) {
+        headers[`OpenAI-Organization`] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Build request body
+
+    body <- list()
+    body[["file"]] <- httr::upload_file(file)
+    body[["purpose"]] <- purpose
+
+    #---------------------------------------------------------------------------
+    # Make a request and verify its result
+
+    result <- httr::POST(
+        url = base_url,
+        httr::content_type_json(),
+        httr::add_headers(.headers = headers),
+        body = body,
+        encode = "multipart"
+    )
+
+    verify_mime_type(result)
+
+    httr::stop_for_status(result)
+
+    #---------------------------------------------------------------------------
+    # Parse the result of the request
+
+    result %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+}
