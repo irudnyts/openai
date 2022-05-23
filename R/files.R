5d5fb16d48541a286fadd527a3fb939952d4e0e0
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-05-23 11:23:06 +0200

    Add `delete_file()`, `retrieve_file()`, and `retreive_file_content()` functions

--- R/files.R
+++ R/files.R
@@ -141,3 +141,192 @@ upload_file <- function(
         jsonlite::fromJSON(flatten = TRUE)
 
 }
+
+#' @export
+delete_file <- function(
+        file_id,
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        assertthat::is.string(file_id),
+        assertthat::noNA(file_id)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build parameters of the request
+
+    base_url <- glue::glue("https://api.openai.com/v1/files/{file_id}")
+
+    headers <- c(
+        Authorization = paste("Bearer", openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        headers["OpenAI-Organization"] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Make a request and verify its result
+
+    result <- httr::DELETE(
+        url = base_url,
+        httr::content_type_json(),
+        httr::add_headers(.headers = headers),
+        encode = "json"
+    )
+
+    verify_mime_type(result)
+
+    httr::stop_for_status(result)
+
+    #---------------------------------------------------------------------------
+    # Parse the result of the request
+
+    result %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+}
+
+#' @export
+retrieve_file <- function(
+        file_id,
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        assertthat::is.string(file_id),
+        assertthat::noNA(file_id)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build parameters of the request
+
+    base_url <- glue::glue("https://api.openai.com/v1/files/{file_id}")
+
+    headers <- c(
+        Authorization = paste("Bearer", openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        headers["OpenAI-Organization"] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Make a request and verify its result
+
+    result <- httr::GET(
+        url = base_url,
+        httr::content_type_json(),
+        httr::add_headers(.headers = headers),
+        encode = "json"
+    )
+
+    verify_mime_type(result)
+
+    httr::stop_for_status(result)
+
+    #---------------------------------------------------------------------------
+    # Parse the result of the request
+
+    result %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+}
+
+# Please note that only output files are allowed to be downloaded, not the
+# output ones.
+#' @export
+retrieve_file_content <- function(
+        file_id,
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        assertthat::is.string(file_id),
+        assertthat::noNA(file_id)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build parameters of the request
+
+    base_url <- glue::glue("https://api.openai.com/v1/files/{file_id}/content")
+
+    headers <- c(
+        Authorization = paste("Bearer", openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        headers["OpenAI-Organization"] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Make a request and verify its result
+
+    result <- httr::GET(
+        url = base_url,
+        httr::content_type_json(),
+        httr::add_headers(.headers = headers),
+        encode = "json"
+    )
+
+    verify_mime_type(result)
+
+    httr::stop_for_status(result)
+
+    #---------------------------------------------------------------------------
+    # Parse the result of the request
+
+    result %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+}
