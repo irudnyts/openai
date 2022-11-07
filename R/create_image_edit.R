ae3810d9e17bdce6aca0863b8f6fc8e7c23ab14d
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-11-07 21:12:28 +0100

    Scafold functions for DALL-E endpoint

---
+++ R/create_image_edit.R
@@ -0,0 +1,13 @@
+create_image_edit <- function(
+        image,
+        mask,
+        prompt,
+        n = 1,
+        size = c("1024x1024", "256x256", "512x512"),
+        response_format = c("url", "b64_json"),
+        user = NULL,
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+}
---
+++ R/create_image_variation.R
@@ -0,0 +1,11 @@
+create_image_variation <- function(
+        image,
+        n = 1,
+        size = c("1024x1024", "256x256", "512x512"),
+        response_format = c("url", "b64_json"),
+        user = NULL,
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+}
