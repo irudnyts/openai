2cd874534e6f3c33ca6124cad091259150836c24
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-11-07 22:21:46 +0100

    Add function body without argument validation

--- R/create_image.R
+++ R/create_image.R
@@ -8,4 +8,67 @@ create_image <- function(
         openai_organization = NULL
 ) {
 
+    size <- match.arg(size)
+    response_format <- match.arg(response_format)
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    #---------------------------------------------------------------------------
+    # Build path parameters
+
+    task <- "images/generations"
+
+    base_url <- glue::glue("https://api.openai.com/v1/{task}")
+
+    headers <- c(
+        "Authorization" = paste("Bearer", openai_api_key),
+        "Content-Type" = "application/json"
+    )
+
+    if (!is.null(openai_organization)) {
+        headers["OpenAI-Organization"] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Build request body
+
+    body <- list()
+    body[["prompt"]] <- prompt
+    body[["n"]] <- n
+    body[["size"]] <- size
+    body[["response_format"]] <- response_format
+    body[["user"]] <- user
+
+    #---------------------------------------------------------------------------
+    # Make a request and parse it
+
+    response <- httr::POST(
+        url = base_url,
+        httr::add_headers(.headers = headers),
+        body = body,
+        encode = "json"
+    )
+
+    verify_mime_type(response)
+
+    parsed <- response %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+    #---------------------------------------------------------------------------
+    # Check whether request failed and return parsed
+
+    if (httr::http_error(response)) {
+        paste0(
+            "OpenAI API request failed [",
+            httr::status_code(response),
+            "]:\n\n",
+            parsed$error$message
+        ) %>%
+            stop(call. = FALSE)
+    }
+
+    parsed
+
 }
