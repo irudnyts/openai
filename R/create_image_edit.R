976ef54108f2c4961b7a6b4cb1b61bcbe5ae483a
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-11-08 22:45:38 +0100

    Develop `create_image_edit()` and `create_image_variation()`

--- R/create_image_edit.R
+++ R/create_image_edit.R
@@ -10,4 +10,122 @@ create_image_edit <- function(
         openai_organization = NULL
 ) {
 
+    size <- match.arg(size)
+    response_format <- match.arg(response_format)
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        assertthat::is.string(image),
+        assertthat::noNA(image),
+        file.exists(image),
+        assertthat::is.readable(image)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(mask),
+        assertthat::noNA(mask),
+        file.exists(mask),
+        assertthat::is.readable(mask)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(prompt),
+        assertthat::noNA(prompt)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.count(n)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(size),
+        assertthat::noNA(size)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(response_format),
+        assertthat::noNA(response_format)
+    )
+
+    if (!is.null(user)) {
+        assertthat::assert_that(
+            assertthat::is.string(user),
+            assertthat::noNA(user)
+        )
+    }
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build path parameters
+
+    task <- "images/edits"
+
+    base_url <- glue::glue("https://api.openai.com/v1/{task}")
+
+    headers <- c(
+        "Authorization" = paste("Bearer", openai_api_key),
+        "Content-Type" = "multipart/form-data"
+    )
+
+    if (!is.null(openai_organization)) {
+        headers["OpenAI-Organization"] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Build request body
+
+    body <- list()
+    body[["image"]] <- httr::upload_file(image)
+    body[["mask"]] <- httr::upload_file(mask)
+    body[["prompt"]] <- prompt
+    body[["n"]] <- n
+    body[["size"]] <- size
+    body[["response_format"]] <- response_format
+    body[["user"]] <- user
+
+    #---------------------------------------------------------------------------
+    # Make a request and parse it
+
+    response <- httr::POST(
+        url = base_url,
+        httr::add_headers(.headers = headers),
+        body = body,
+        encode = "multipart"
+    )
+
+    verify_mime_type(response)
+
+    parsed <- response %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+    #---------------------------------------------------------------------------
+    # Check whether request failed and return parsed
+
+    if (httr::http_error(response)) {
+        paste0(
+            "OpenAI API request failed [",
+            httr::status_code(response),
+            "]:\n\n",
+            parsed$error$message
+        ) %>%
+            stop(call. = FALSE)
+    }
+
+    parsed
+
+
 }
--- R/create_image_variation.R
+++ R/create_image_variation.R
@@ -8,4 +8,108 @@ create_image_variation <- function(
         openai_organization = NULL
 ) {
 
+    size <- match.arg(size)
+    response_format <- match.arg(response_format)
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        assertthat::is.string(image),
+        assertthat::noNA(image),
+        file.exists(image),
+        assertthat::is.readable(image)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.count(n)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(size),
+        assertthat::noNA(size)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(response_format),
+        assertthat::noNA(response_format)
+    )
+
+    if (!is.null(user)) {
+        assertthat::assert_that(
+            assertthat::is.string(user),
+            assertthat::noNA(user)
+        )
+    }
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build path parameters
+
+    task <- "images/variations"
+
+    base_url <- glue::glue("https://api.openai.com/v1/{task}")
+
+    headers <- c(
+        "Authorization" = paste("Bearer", openai_api_key),
+        "Content-Type" = "multipart/form-data"
+    )
+
+    if (!is.null(openai_organization)) {
+        headers["OpenAI-Organization"] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Build request body
+
+    body <- list()
+    body[["image"]] <- httr::upload_file(image)
+    body[["n"]] <- n
+    body[["size"]] <- size
+    body[["response_format"]] <- response_format
+    body[["user"]] <- user
+
+    #---------------------------------------------------------------------------
+    # Make a request and parse it
+
+    response <- httr::POST(
+        url = base_url,
+        httr::add_headers(.headers = headers),
+        body = body,
+        encode = "multipart"
+    )
+
+    verify_mime_type(response)
+
+    parsed <- response %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+    #---------------------------------------------------------------------------
+    # Check whether request failed and return parsed
+
+    if (httr::http_error(response)) {
+        paste0(
+            "OpenAI API request failed [",
+            httr::status_code(response),
+            "]:\n\n",
+            parsed$error$message
+        ) %>%
+            stop(call. = FALSE)
+    }
+
+    parsed
+
+
 }
