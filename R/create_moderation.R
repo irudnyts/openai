c0ea764c456a11942adf8106821d194f8f289a3e
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2022-11-23 11:52:22 +0100

    Add function `create_moderation()`

---
+++ R/create_moderation.R
@@ -0,0 +1,89 @@
+create_moderation <- function(
+        input,
+        model = c("text-moderation-stable", "text-moderation-latest"),
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+    model <- match.arg(model)
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        is.character(input),
+        assertthat::noNA(input)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(model),
+        assertthat::noNA(model)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build path parameters
+
+    task <- "moderations"
+
+    base_url <- glue::glue("https://api.openai.com/v1/{task}")
+
+    headers <- c(
+        "Authorization" = paste("Bearer", openai_api_key),
+        "Content-Type" = "application/json"
+    )
+
+    if (!is.null(openai_organization)) {
+        headers["OpenAI-Organization"] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Build request body
+
+    body <- list()
+    body[["input"]] <- input
+    body[["model"]] <- model
+
+    #---------------------------------------------------------------------------
+    # Make a request and parse it
+
+    response <- httr::POST(
+        url = base_url,
+        httr::add_headers(.headers = headers),
+        body = body,
+        encode = "json"
+    )
+
+    verify_mime_type(response)
+
+    parsed <- response %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+    #---------------------------------------------------------------------------
+    # Check whether request failed and return parsed
+
+    if (httr::http_error(response)) {
+        paste0(
+            "OpenAI API request failed [",
+            httr::status_code(response),
+            "]:\n\n",
+            parsed$error$message
+        ) %>%
+            stop(call. = FALSE)
+    }
+
+    parsed
+
+}
