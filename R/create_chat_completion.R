38d820e9533b750a63e36e44932add3b594f7875
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-03-02 23:15:15 +0100

    Develop `create_chat_compeletion()`

---
+++ R/create_chat_completion.R
@@ -0,0 +1,179 @@
+#' @export
+create_chat_completion<- function(
+        model = c("gpt-3.5-turbo", "gpt-3.5-turbo-0301"),
+        messages = NULL,
+        temperature = 1,
+        top_p = 1,
+        n = 1,
+        stream = FALSE,
+        stop = NULL,
+        max_tokens = NULL,
+        presence_penalty = 0,
+        frequency_penalty = 0,
+        logit_bias = NULL,
+        user = NULL,
+        openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+        openai_organization = NULL
+) {
+
+    model <- match.arg(model)
+
+    #---------------------------------------------------------------------------
+    # Validate arguments
+
+    assertthat::assert_that(
+        assertthat::is.string(model),
+        assertthat::noNA(model)
+    )
+
+    if (!is.null(messages)) {
+        assertthat::assert_that(
+            is.list(messages)
+        )
+    }
+
+    assertthat::assert_that(
+        assertthat::is.number(temperature),
+        assertthat::noNA(temperature),
+        value_between(temperature, 0, 2)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.number(top_p),
+        assertthat::noNA(top_p),
+        value_between(top_p, 0, 1)
+    )
+
+    if (both_specified(temperature, top_p)) {
+        warning(
+            "It is recommended NOT to specify temperature and top_p at a time."
+        )
+    }
+
+    assertthat::assert_that(
+        assertthat::is.count(n)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.flag(stream),
+        assertthat::noNA(stream),
+        is_false(stream)
+    )
+
+    if (!is.null(stop)) {
+        assertthat::assert_that(
+            is.character(stop),
+            assertthat::noNA(stop),
+            length_between(stop, 1, 4)
+        )
+    }
+
+
+    if (!is.null(max_tokens)) {
+        assertthat::assert_that(
+            assertthat::is.count(max_tokens)
+        )
+    }
+
+    assertthat::assert_that(
+        assertthat::is.number(presence_penalty),
+        assertthat::noNA(presence_penalty),
+        value_between(presence_penalty, -2, 2)
+    )
+
+    assertthat::assert_that(
+        assertthat::is.number(frequency_penalty),
+        assertthat::noNA(frequency_penalty),
+        value_between(frequency_penalty, -2, 2)
+    )
+
+    if (!is.null(logit_bias)) {
+        assertthat::assert_that(
+            is.list(logit_bias)
+        )
+    }
+
+    if (!is.null(user)) {
+        assertthat::assert_that(
+            assertthat::is.string(user),
+            assertthat::noNA(user)
+        )
+    }
+
+    assertthat::assert_that(
+        assertthat::is.string(openai_api_key),
+        assertthat::noNA(openai_api_key)
+    )
+
+    if (!is.null(openai_organization)) {
+        assertthat::assert_that(
+            assertthat::is.string(openai_organization),
+            assertthat::noNA(openai_organization)
+        )
+    }
+
+    #---------------------------------------------------------------------------
+    # Build path parameters
+
+    task <- "chat/completions"
+
+    base_url <- glue::glue("https://api.openai.com/v1/{task}")
+
+    headers <- c(
+        "Authorization" = paste("Bearer", openai_api_key),
+        "Content-Type" = "application/json"
+    )
+
+    if (!is.null(openai_organization)) {
+        headers["OpenAI-Organization"] <- openai_organization
+    }
+
+    #---------------------------------------------------------------------------
+    # Build request body
+
+    body <- list()
+    body[["model"]] <- model
+    body[["messages"]] <- messages
+    body[["temperature"]] <- temperature
+    body[["top_p"]] <- top_p
+    body[["n"]] <- n
+    body[["stream"]] <- stream
+    body[["stop"]] <- stop
+    body[["max_tokens"]] <- max_tokens
+    body[["presence_penalty"]] <- presence_penalty
+    body[["frequency_penalty"]] <- frequency_penalty
+    body[["logit_bias"]] <- logit_bias
+    body[["user"]] <- user
+
+    #---------------------------------------------------------------------------
+    # Make a request and parse it
+
+    response <- httr::POST(
+        url = base_url,
+        httr::add_headers(.headers = headers),
+        body = body,
+        encode = "json"
+    )
+
+    verify_mime_type(response)
+
+    parsed <- response %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+    #---------------------------------------------------------------------------
+    # Check whether request failed and return parsed
+
+    if (httr::http_error(response)) {
+        paste0(
+            "OpenAI API request failed [",
+            httr::status_code(response),
+            "]:\n\n",
+            parsed$error$message
+        ) %>%
+            stop(call. = FALSE)
+    }
+
+    parsed
+
+}
