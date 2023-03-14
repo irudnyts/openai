897a49dcfebcb5fb37f8526a368327ed2064e714
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-03-15 00:20:42 +0100

    Update example of `create_embedding()`

--- R/create_embedding.R
+++ R/create_embedding.R
@@ -20,7 +20,7 @@
 #'   a given input.
 #' @examples \dontrun{
 #' create_embedding(
-#'     model = "text-similarity-babbage-001",
+#'     model = "text-embedding-ada-002",
 #'     input = c(
 #'         "Ah, it is so boring to write documentation",
 #'         "But examples are really crucial"
