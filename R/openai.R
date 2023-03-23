14cafb720d461b591ee8eb5f2b6d089c5ccaf4eb
Markus Skyttner <markussk@mail.com>
2023-03-23 21:25:49 +0100

    add data with a named vector of iso 639-1 language codes (taken from wikipedia)

--- R/openai.R
+++ R/openai.R
@@ -1,2 +1,12 @@
 #' @importFrom lifecycle deprecated
 NULL
+
+#' ISO 639-1 two-letter language codes
+#'
+#' A named vector of two-letter language codes.
+#'
+#' @name iso_languages
+#' @docType data
+#' @references \url{https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes}
+#' @keywords data
+NULL
