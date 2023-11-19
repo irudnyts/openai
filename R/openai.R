6b18c67f05ae62fdd2522c38b48ce7e104e8b100
Iegor Rudnytskyi <yegor.rudnytskiy@gmail.com>
2023-11-19 20:56:11 +0100

    Copy arguments from constructor to its alias

--- R/openai.R
+++ R/openai.R
@@ -6,7 +6,12 @@ OpenAI <- function(
         api_key = NULL,
         organization = NULL,
         base_url = NULL,
-        max_retries = NULL
+        # timeout = NULL,
+        max_retries = NULL #,
+        # default_headers = NULL,
+        # default_query = NULL,
+        # http_client = NULL,
+        # .strict_response_validation = NULL
 ) {
     openai$new(api_key, organization)
 }
